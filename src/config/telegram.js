0a73f8be035849ef0b64247ce891cf26b534fbe9
kaine1973 <32561008+kaine1973@users.noreply.github.com>
2024-12-26 13:57:29 +0800

    Update telegram.js

--- src/config/telegram.js
+++ src/config/telegram.js
@@ -2,6 +2,6 @@ const TelegramBot = require("node-telegram-bot-api");
 
 const token = process.env.TELEGRAM_TOKEN;
 
-const Bot = new TelegramBot(token, { polling: true });
+const Bot = new TelegramBot(token, { polling: false });
 
 module.exports = { Bot };
